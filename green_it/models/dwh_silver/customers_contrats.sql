5408f4d93fcd3041a8785307e3902e306f520266
Mistercrv <crvgbale@gmail.com>
2026-01-13 11:51:56 +0100

    creation d'un dwh dwh_silver et d'un dwh view

---
+++ green_it/models/dwh_silver/customers_contrats.sql
@@ -0,0 +1,23 @@
+
+
+with  vw_customers_contrats as (
+SELECT
+    c.customer_id,
+    c.gender,
+    c.age,
+    c.state,
+    c.is_group,
+    c.group_size,
+    ct.contract_type,
+    ct.payment_method,
+    ct.account_length_months,
+    ct.monthly_charge,
+    ct.total_charges,
+    ct.unlimited_data_plan
+FROM {{ ref('customers') }} c
+LEFT JOIN {{ ref('contrats') }} ct
+    ON c.customer_id = ct.customer_id
+
+)
+
+select * from vw_customers_contrats
---
+++ green_it/models/dwh_silver/global_customer.sql
@@ -0,0 +1,31 @@
+
+
+with vw_global_customer as (
+
+    SELECT
+        c.customer_id,
+        c.gender,
+        c.age,
+        c.state,
+        c.is_group,
+        c.group_size,
+
+        ct.contract_type,
+        ct.payment_method,
+        ct.account_length_months,
+        ct.avg_monthly_gb,
+        ct.monthly_charge,
+        ct.total_charges,
+
+        s.churn_label,
+        s.churn_category,
+        s.churn_reason
+    FROM {{ ref('customers') }} c
+    LEFT JOIN {{ ref('contrats') }} ct
+        ON c.customer_id = ct.customer_id
+    LEFT JOIN {{ ref('status') }} s
+        ON c.customer_id = s.customer_id
+
+)
+
+select * from vw_global_customer
